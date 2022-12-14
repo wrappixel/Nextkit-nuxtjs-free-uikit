1b74a73cbec029ae99faf4a7f99be51a659f6eec
minh97 <minhnguyen@beeketing.net>
2022-12-14 21:26:22 +0700

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
