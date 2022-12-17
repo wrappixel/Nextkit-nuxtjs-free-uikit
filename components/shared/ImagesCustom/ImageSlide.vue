82ee2aac78e58fb0c7ed9b019df14877b622ee40
namnguyenacon <namnguyen@beeketing.net>
2022-12-17 23:13:50 +0700

    creat page detail

--- components/shared/ImagesCustom/ImageSlide.vue
+++ components/shared/ImagesCustom/ImageSlide.vue
@@ -1,17 +1,29 @@
 <template>
-  <v-sheet class="mx-auto" max-width="500">
+  <v-sheet class="my-16 p-br4-imp" elevation="5" max-width="450">
     <v-slide-group
       v-model="image"
-      class="pa-4"
+      class="p-br16"
       active-class="success"
       show-arrows
+      center-active
+      light
     >
       <v-slide-item
         v-for="i in images"
         :key="i.id"
         v-slot="{ active, toggle }"
+        class="mr-8"
       >
-        <v-img :src="i.src" width="300" aspect-ratio="0.8" :transition="false"/>
+        <v-card
+          width="100"
+          height="150"
+          @click="toggle"
+          :class="active ? '' : 'image-deactive'"
+        >
+          <v-scale-transition>
+            <v-img :src="i.src" height="150"/>
+          </v-scale-transition>
+        </v-card>
       </v-slide-item>
     </v-slide-group>
   </v-sheet>
@@ -40,11 +52,13 @@ export default {
       }
     },
     image: {
-      handler: function (val, old) {
-        if (val !== old) {
-          this.$emit('update:this.current', val)
-        }
-
+      handler: function (val) {
+        this.$emit('update:current', val)
+      }
+    },
+    current: {
+      handler: function (val) {
+        this.image = val
       }
     }
   }
--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -2,40 +2,52 @@
   <div class="product-detail">
     <v-container>
       <v-row>
-        <v-col cols="12" lg="9" md="8" xs="12">
+        <v-col cols="12" lg="8" md="8" sm="12">
           <!--    Carousel    -->
           <div class="mb-16">
-            <v-carousel hide-delimiters>
+            <v-carousel hide-delimiters v-model="currentImg" class="elevation-8">
               <v-carousel-item v-for="i in productDetail.images" :key="i.id">
                 <v-img :src="i.src"></v-img>
               </v-carousel-item>
             </v-carousel>
-            <ImageSlide :images="productDetail.images" @onSelectImage="(id) => currentImg = id"/>
+            <ImageSlide :images="productDetail.images" @onSelectImage="(id) => currentImg = id" :current.sync="currentImg"/>
           </div>
 
           <!--    Description    -->
-          <div>
+          <div class="product-detail__description">
             <h1>{{ $t('description') }}</h1>
-            <div v-html="productDetail.long_description"></div>
+            <div>
+              <div v-html="productDetail.long_description"></div>
+            </div>
           </div>
 
 
         </v-col>
-        <v-col cols="12" lg="3" md="4" xs="12">
+        <v-col cols="12" lg="4" md="4" sm="12">
           <div>
             <h1>{{ productDetail.title }}</h1>
-            <div class="d-flex product-price">
-              <p class="text-capitalize mr-8">{{ $t('from_price') }}:</p>
-              <h4 class="font-weight-bold">{{ productDetail.price | formatPrice }} </h4>
+            <div class="product-price">
+              <div class="d-flex">
+                <p class="text-capitalize mr-8 my-0">{{ $t('from_price') }}:</p>
+                <h4 class="font-weight-bold">{{ productDetail.price | formatPrice }} </h4>
+              </div>
+              <v-rating
+                v-model="rating"
+                background-color="orange lighten-3"
+                color="orange"
+              ></v-rating>
             </div>
-            <div class="d-flex product-price" v-if="productDetail.shot_description">
-              <p>{{ productDetail.shot_description }}</p>
+
+            <div class="d-flex product-price" v-if="productDetail.short_description">
+              <p class="black--text">{{ productDetail.short_description }}</p>
             </div>
             <!--    Button register tour        -->
             <div>
               <v-btn class="w-100" color="primary"
                      @click="$vuetify.goTo('#contact-form', {duration: 1000, easing:'easeInQuad'})">
-                {{ $t('checkout') }}
+                <span class="font-weight-bold">
+                  {{ $t('checkout') }}
+                </span>
               </v-btn>
             </div>
           </div>
@@ -61,7 +73,8 @@ export default {
   data() {
     return {
       productDetail: {},
-      currentImg: null
+      currentImg: null,
+      rating: 4
     }
   },
   computed: {
@@ -77,11 +90,12 @@ export default {
   },
   methods: {
     ...mapActions('product', {
-      fetchDetailProduct: "fetchDetailProduct"
+      fetchDetailProduct: "fetchDetailProduct",
+      fetchAllProducts: 'fetchAllProducts'
     }),
     async init() {
       const id = this.$route.params.id
-      const promise = [this.fetchDetailProduct({id: id})]
+      const promise = [this.fetchDetailProduct({id: id}),this.fetchAllProducts()]
       await Promise.all(promise)
       this.productDetail = cloneDeep(this.product)
     }
